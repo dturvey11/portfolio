7b9bf2f300ac8f4c5d73ac663e2ab3fbab1bebe2
Devin Turvey <dturvey@Devins-MacBook-Pro-2.local>
2023-08-14 18:05:58 -0400

    Add skills contactand dark mode

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,10 +7,12 @@ import {BsArrowRight, BsLinkedin} from "react-icons/bs";
 import {HiDownload} from "react-icons/hi";
 import { FaGithubSquare } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Intro() {
 
     const { ref } =useSectionInView('Home', 0.5);
+    const {setActiveSection, setTimeOfLastClick} = useActiveSectionContext()
     
   return (
     <section ref= {ref} id="home" className= "mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
@@ -55,18 +57,21 @@ export default function Intro() {
         transition={{
             delay: 0.1,
         }}>
-            <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+            <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition" 
+            onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now())}}>Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
             </Link>
 
-            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/CV.pdf" download>Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
+            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10" href="/CV.pdf" download>Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
             </a>
 
-            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
              href="https://www.linkedin.com/in/devin-turvey-052145211" target="_blank">
                 <BsLinkedin />
             </a>
 
-            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
             href="https://github.com/dturvey11" target="_blank">
                 <FaGithubSquare />
             </a>
--- next.config.js
+++ next.config.js
@@ -5,9 +5,12 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: 'images.unsplash.com',
-            }
-        ]
-    }
-}
+            },
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
+};
 
 module.exports = nextConfig

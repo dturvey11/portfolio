62faf3e75ad0ca70ff8481ae3840879e1b1a88d4
Devin Turvey <dturvey@Devins-MacBook-Pro-2.local>
2023-08-14 22:18:47 -0400

    Fix bug

--- components/intro.tsx
+++ components/intro.tsx
@@ -46,7 +46,7 @@ export default function Intro() {
         initial={{opacity: 0, y: 100}}
         animate={{opacity: 1, y:0}}>
             <span className="font-bold">Hello, I&apos;m Devin.</span> I&apos;m a{" "}
-            <span className="font-bold">Junior Software Developer</span> with a lot of{" "}
+            <span className="font-bold">Junior Software Developer</span> with extensive{" "}
             <span className="font-bold">project</span> experience. I enjoy
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>. 
